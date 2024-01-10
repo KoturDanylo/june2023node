7d72a5005ece5b9313b0bdfe6cfae586e531bc91
danylokotur <kotur.danylo@gmail.com>
2024-01-10 17:14:52 +0200

    kn

--- app.js
+++ app.js
@@ -164,6 +164,41 @@ app.delete('/users/:id', async (req, res) => {
     }
 });
 
+app.put('/users/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const {email, age, name} = req.body;
+
+        if (!Number.isInteger(id)) {
+            throw new Error('wrong ID param');
+        }
+
+        if (!age || !Number.isInteger(age) || age <= 0 || age > 100) {
+            throw new Error('wrong age');
+        }
+        if (!email || !email.includes('@')) {
+            throw new Error('wrong email');
+        }
+        if (!name || name.length <= 3) {
+            throw new Error('wrong name');
+        }
+
+        const users = await read();
+        const user = users.find((user) => user.id === id);
+        if (!user) {
+            throw new Error('user not found');
+        }
+        user.name = name;
+        user.age = age;
+        user.email = email;
+
+        await write(users);
+
+        res.status(201).json(user);
+    } catch (e) {
+        res.status(400).json(e.message);
+    }
+});
 
 
 
